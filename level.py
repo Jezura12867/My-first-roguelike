99b9c98563c3a89f90fda0443011d7d739192125
Jezura <169179776+Jezura12867@users.noreply.github.com>
2024-05-07 18:18:52 +0200

    Add files via upload

--- level.py
+++ level.py
@@ -2,30 +2,51 @@ import pygame
 from sprites import Cube
 
 class Walls:
-    def show(screen, player):
+    def show(screen, map_num):
         cubes = pygame.sprite.Group()
         map_tile_size = 64
-        map = [
-            "X                             X",
-            "                              X",
-            "                              X",
-            "                              X",
-            "             XXXX             X",
-            "             XXXX             X",
-            "             XXXX             X",
-            "                              X",
-            "                              X",
-            "                              X",
-            "                              X",
-            "                              X",
-            "                              X",
-            "                              X",
-            "                              X",
-            "                              X",
-            "                              X",
-            "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        map1 = [
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
         ]
-        for cislo_radku, radek in enumerate(map):
+        map2 = [
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                 X             ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+            "                               ",
+        ]
+        maps = [map1, map2]
+        for cislo_radku, radek in enumerate(maps[map_num]):
             for cislo_sloupce, bunka in enumerate(radek):
                 if bunka == "X":
                     x = cislo_sloupce * map_tile_size / 1.3 + 2
@@ -33,8 +54,6 @@ class Walls:
                     cube = Cube((x, y), map_tile_size)
                     cubes.add(cube)
         
-        if cube.rect.colliderect(player):
-            return True
 
         cubes.draw(screen)
         
